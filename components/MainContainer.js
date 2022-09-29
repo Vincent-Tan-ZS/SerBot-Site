27a59521de57918aed6baef4ae4851834c548f6f
Vincent <vintzs98@yahoo.com>
2022-09-29 22:02:49 +0800

    v0.2
    Commands page (WIP)

---
+++ components/MainContainer.js
@@ -0,0 +1,8 @@
+import {Container, styled} from "@mui/material";
+
+const MainContainer = styled(Container)`
+	background-color: #191919;
+	height: 100vh;
+`;
+
+export default MainContainer;
--- pages/commands.js
+++ pages/commands.js
@@ -1,29 +1,84 @@
+import {Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
 import React from "react";
+import MainContainer from "../components/MainContainer";
+import styles from '../styles/Commands.module.scss';
 
 function Commands(props) {
+	const { list } = props;
+
 	const [commandList, setCommandList] = React.useState([]);
 
 	React.useEffect(() => {
-		fetch(`./api/command`).then((resp) => {
-			resp.json().then((respData) => {
-				setCommandList(respData.data);	
-			})
-			.catch((reason) => {
-				console.log(reason);
-			});
-		})
-		.catch((reason) => {
-			console.log(reason);
+		let _list = list.map(li => {
+			let _usage = li.Usage.map(u => `ser ${li.List[0]} ${u}`.trim());
+
+			return {
+				Title: li.Title,
+				List: li.List,
+				Description: li.Description,
+				Usage: _usage
+			}
 		});
-	}, []);
+
+		setCommandList(_list);
+	}, [list]);
 
 	return (
-		<ul>
-			{
-				commandList.map(l => <li>{l.Title}</li>)
-			}
-		</ul>
+		<MainContainer>
+			<Box />
+			<TableContainer>
+				<Table className={styles.commandsTable}>
+					<TableHead>
+						<TableRow>
+							<TableCell>Title</TableCell>
+							<TableCell>Commands</TableCell>
+							<TableCell>Description</TableCell>
+							<TableCell>Usage</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{
+							commandList.length > 0 &&
+							commandList.map(li => {
+								return (
+									<TableRow key={`command-${li.Title}`}>
+										<TableCell size="small">{li.Title}</TableCell>
+										<TableCell size="small">{li.List.join()}</TableCell>
+										<TableCell size="small">{li.Description}</TableCell>
+										<TableCell size="small">{li.Usage.join(" | ")}</TableCell>
+									</TableRow>
+								)
+							})
+						}
+					</TableBody>
+				</Table>
+			</TableContainer>	
+		</MainContainer>
 	)
+}
+
+export const getServerSideProps = async () => {
+	let data = [];
+
+	try
+	{
+		const resp = await fetch(`http://${process.env.DOMAIN_NAME}:${process.env.DOMAIN_PORT}/api/command`);
+		const respData = await resp.json();
+
+		data = respData.data;
+	}
+	catch (e)
+	{
+		console.log(e.message);
+	}
+	finally
+	{
+		return {
+			props: {
+				list: data
+			}
+		};
+	}
 }
 
 export default Commands;
