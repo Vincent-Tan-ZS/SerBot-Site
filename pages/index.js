e45d5cfbb740673b92e7eb810473c7b633ad87c3
Vincent <vintzs98@yahoo.com>
2022-10-05 20:55:51 +0800

    Home Screen

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
+import {Box, Button, styled, Typography} from "@mui/material";
+import Link from "next/link";
 import AppBody from "../components/AppBody";
+import { ViewList as ViewListIcon } from "@mui/icons-material";
+
+const HeaderBox = styled(Box)`
+  display: flex;
+  align-items: center;
+  justify-contents: center;
+
+  flex-direction: column;
+`;
 
 function Home() {
   return (
     <>
       <AppBody>
-        <p>Test</p>
+        <HeaderBox>
+          <Typography variant={"h1"}>
+            W.I.P.
+          </Typography>
+          <Typography variant={"h3"}>
+            Sorry, this page is still a Work In Progress :(
+          </Typography>
+          <Link href="/commands" passHref>
+            <Button variant={"contained"}>
+              <ViewListIcon />
+              Commands
+            </Button>
+          </Link>
+        </HeaderBox>
       </AppBody>
     </>
   )
